d4f80de179e338b8a142e0d67967b66de4ff527a
Takeru Sato <midium.size@gmail.com>
2016-02-09 18:04:31 +0900

    Change the type annotation for Action

--- core/src/main/scala/agni/Functions.scala
+++ core/src/main/scala/agni/Functions.scala
@@ -4,6 +4,5 @@ import com.datastax.driver.core.Session
 import cats.data.{ Xor, Kleisli }
 
 trait Functions {
-  def withSession[F[_], A](f: Session => F[Xor[Throwable, A]]): Action[F, A] =
-    Kleisli.function[Lambda[a => F[Xor[Throwable, a]]], Session, A](f)
+  def withSession[F[_], A](f: Session => F[A]): Action[F, A] = Action(f)
 }
--- core/src/main/scala/agni/package.scala
+++ core/src/main/scala/agni/package.scala
@@ -1,23 +1,9 @@
-import cats.FlatMap
-import cats.data.{ Xor, Kleisli }
-import cats.implicits._
+import cats.data.Kleisli
 import com.datastax.driver.core.Session
-
-import scala.concurrent.{ ExecutionContext, Future }
 
 package object agni {
 
-  type Action[F[_], U] = Kleisli[Lambda[a => F[Throwable Xor a]], Session, U]
-
-  implicit def resultFlatMap(implicit ec: ExecutionContext) = new FlatMap[Lambda[a => Future[Throwable Xor a]]] {
-    override def flatMap[A, B](fa: Future[Throwable Xor A])(f: (A) => Future[Throwable Xor B]): Future[Throwable Xor B] = fa.flatMap {
-      case x @ Xor.Left(e) => Future(x)
-      case Xor.Right(v) => f(v)
-    }
-    override def map[A, B](fa: Future[Throwable Xor A])(f: (A) => B): Future[Throwable Xor B] = fa.map {
-      case x @ Xor.Left(e) => x
-      case Xor.Right(v) => f(v).right
-    }
-  }
+  type Action[F[_], A] = Kleisli[F, Session, A]
+  val Action = Kleisli
 
 }
