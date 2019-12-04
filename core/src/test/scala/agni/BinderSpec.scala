6c7bb3065681b43a0d096c96393d29fa29d4758f
Takeru Sato <type.in.type@gmail.com>
2019-12-05 01:18:14 +0900

    fix: change to use each result of Binder[T]

--- core/src/test/scala/agni/BinderSpec.scala
+++ core/src/test/scala/agni/BinderSpec.scala
@@ -1,8 +1,14 @@
 package agni
 
+import com.datastax.oss.driver.api.core.ProtocolVersion
+import com.datastax.oss.driver.api.core.`type`.codec.TypeCodecs
+import com.datastax.oss.driver.api.core.cql.BoundStatement
+import org.mockito.Mockito._
+import org.mockito.ArgumentMatchers.{ `eq` => eqTo }
 import org.scalatest.Assertion
+import org.scalatestplus.mockito.MockitoSugar
 
-class BinderSpec extends TypedSuite {
+class BinderSpec extends TypedSuite with MockitoSugar {
   import TypedSuite._
 
   def checkType[A: Binder]: Assertion = {
@@ -35,4 +41,23 @@ class BinderSpec extends TypedSuite {
   test("Binder[T21]")(checkType[T21])
   test("Binder[T22]")(checkType[T22])
   test("Binder[T22_2]")(checkType[T22_2])
+
+  test("binding values as a tuple") {
+    val a = mock[BoundStatement]
+    val b = mock[BoundStatement]
+    val c = mock[BoundStatement]
+    val d = mock[BoundStatement]
+    val e = mock[BoundStatement]
+
+    val v = ProtocolVersion.DEFAULT
+
+    when(a.setBytesUnsafe(eqTo(0), eqTo(TypeCodecs.ASCII.encode("a", v)))).thenReturn(b)
+    when(b.setBytesUnsafe(eqTo(1), eqTo(TypeCodecs.INT.encode(1, v)))).thenReturn(c)
+    when(c.setBytesUnsafe(eqTo(2), eqTo(TypeCodecs.BIGINT.encode(2L, v)))).thenReturn(d)
+    when(d.setBytesUnsafe(eqTo(3), eqTo(TypeCodecs.ASCII.encode("z", v)))).thenReturn(e)
+
+    val Right(s) = Binder[(String, Int, Long, String)].apply(a, v, ("a", 1, 2L, "z"))
+
+    assert(e === s)
+  }
 }
